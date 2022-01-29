f7d444f2550356b45d2bfa82b846eb1fbf233a93
lightsaber_home <bestornot04@naver.com>
2022-01-30 03:25:57 +0900

    almost finished

--- SpringProject/src/main/java/com/example/lesson05/Lesson05QuizController.java
+++ SpringProject/src/main/java/com/example/lesson05/Lesson05QuizController.java
@@ -1,13 +1,49 @@
 package com.example.lesson05;
 
+import java.util.Date;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
+import com.example.lesson05.bo.WeatherHistoryBO;
+import com.example.lesson05.model.WeatherHistory;
 
+@RequestMapping("/lesson05")
 @Controller
 public class Lesson05QuizController {
-	@RequestMapping("/lesson05/quiz")
-	public String test() {
-		return "lesson05/ex01";
+	@Autowired
+	private WeatherHistoryBO weatherHistoryBO;
+	
+	@RequestMapping("/quiz05")
+	public String reports(Model model) {
+		List<WeatherHistory> searchResult = weatherHistoryBO.selectAll();
+		model.addAttribute("searchResult", searchResult);
+		return "lesson05/quiz05/reports";
+	}
+	
+	@RequestMapping("/quiz05/insert")
+	public String displayInsert(Model model) {
+		String actionUrl = "/lesson05/quiz05/insert_1";
+		model.addAttribute("actionUrl", actionUrl);
+		return "lesson05/quiz05/insert";
+	}
+	
+	@PostMapping("/quiz05/insert_1")
+	public String doInsert(
+			@RequestParam("date") String date,
+			@RequestParam("weather") String weather,
+			@RequestParam("temperatures") Double temperatures,
+			@RequestParam("precipitation") Double precipitation,
+			@RequestParam("microDust") String microDust,
+			@RequestParam("windSpeed") Double windSpeed
+			) {
+		weatherHistoryBO.insert(date, weather, temperatures, precipitation, microDust, windSpeed);
+		
+		return "redirect:/lesson05/quiz05";
 	}
 }
